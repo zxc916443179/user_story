608361307b0ba95654afe61681c48900b15f930e
zxc916443179 <zxc916443179@163.com>
2021-03-25 15:37:33 +0800

    add paramscheck

--- UserStory/UserStory/api.py
+++ UserStory/UserStory/api.py
@@ -10,6 +10,10 @@ def APIFunction(func, *args, **kwargs):
             params = request.POST
         elif request.method == 'GET':
             params = request.GET
+        if 'args' not in func.__code__.co_varnames and 'kwargs' not in func.__code__.co_varnames:
+            for var in func.__code__.co_varnames:
+                if not params.__contains__(var):
+                    return APIResponse(code=102, msg=RETCode[102])
         try:
             res = func(**params)
             return APIResponse(content=res, code=0)
@@ -36,6 +40,7 @@ RETCode = {
     100: '操作成功',
 
     101: '未知错误',
+    102: '请求参数不匹配'
 }
 @APIFunction
 def Hello(id):
