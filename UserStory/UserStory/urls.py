5c9d865abc75b304a0e4a322f89c5e0704ae9939
zxc916443179 <zxc916443179@163.com>
2021-03-25 10:42:42 +0800

    init model

--- UserStory/UserStory/urls.py
+++ UserStory/UserStory/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from . import api
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', api.Hello)
 ]
---
+++ UserStory/UserStoryModel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserstorymodelConfig(AppConfig):
+    name = 'UserStoryModel'
