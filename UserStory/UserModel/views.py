e5de8a6d6a84ef0127f776524c3a295d17bdc7cc
zxc916443179 <zxc916443179@163.com>
2021-03-25 23:01:08 +0800

    add logout

--- UserStory/UserModel/views.py
+++ UserStory/UserModel/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from hashlib import md5
 # Create your views here.
+from UserStory.api import APIFunction, APIError
+from .controller import UserController
+@APIFunction
+def log_up(username, password):
+    user = UserController.create(username, password)
+    return user
+
+@APIFunction
+def log_in(username, password, session):
+    users = UserController.get_by_username(username)
+    if len(users) == 0:
+        raise APIError(201)
+    if md5(password.encode('utf-8')).hexdigest() != users[0].password:
+        raise APIError(202)
+    user = users[0]
+    session['user_session'] = user.username
+    return users[0]
+@APIFunction
+def log_out(session):
+    session['user_session'] = None
+    return ''
+
